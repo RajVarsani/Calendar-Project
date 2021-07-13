da2352fee7c434923a7346f6389778835acd392a
Raj varsani <varasaniraj5@gmail.com>
2021-07-13 15:12:19 +0530

    Added widget 2

--- Android/TheHinduCalender/app/src/main/java/com/example/thehinducalender/Widget2.kt
+++ Android/TheHinduCalender/app/src/main/java/com/example/thehinducalender/Widget2.kt
@@ -8,7 +8,7 @@ import android.widget.RemoteViews
 /**
  * Implementation of App Widget functionality.
  */
-class Widget1 : AppWidgetProvider() {
+class Widget2 : AppWidgetProvider() {
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
@@ -16,7 +16,7 @@ class Widget1 : AppWidgetProvider() {
     ) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
-            updateAppWidget1(context, appWidgetManager, appWidgetId)
+            updateAppWidget(context, appWidgetManager, appWidgetId)
         }
     }
 
@@ -29,14 +29,14 @@ class Widget1 : AppWidgetProvider() {
     }
 }
 
-internal fun updateAppWidget1(
+internal fun updateAppWidget(
     context: Context,
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
-//    val widgetText = context.getString(R.string.appwidget_text)
+    val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.widget1)
+    val views = RemoteViews(context.packageName, R.layout.widget2)
 //    views.setTextViewText(R.id.appwidget_text, widgetText)
 
     // Instruct the widget manager to update the widget
