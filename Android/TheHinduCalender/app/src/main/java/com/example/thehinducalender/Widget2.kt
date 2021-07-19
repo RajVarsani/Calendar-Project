79bbf1e8c19e4859018171abc9e18b63e0542e06
Raj varsani <varasaniraj5@gmail.com>
2021-07-19 13:46:08 +0530

    Updated updated widget function in widget2

--- Android/TheHinduCalender/app/src/main/java/com/example/thehinducalender/Widget2.kt
+++ Android/TheHinduCalender/app/src/main/java/com/example/thehinducalender/Widget2.kt
@@ -7,6 +7,7 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import android.widget.RemoteViews
+import com.example.thehinducalender.appwidgetdaos.AppWidgetsDataHandlerDao
 
 /**
  * Implementation of App Widget functionality.
@@ -17,41 +18,36 @@ class Widget2 : AppWidgetProvider() {
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-        // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
         }
     }
 
     override fun onEnabled(context: Context) {
-        // Enter relevant functionality for when the first widget is created
-
         val appWidgetAlarm = AppWidgetAlarm1(context.applicationContext)
-        appWidgetAlarm.startAlarm(1, Widget2::class.java, ACTION_AUTO_UPDATE)
+        appWidgetAlarm.startAlarm(1, Widget2::class.java, ACTION_AUTO_UPDATE_W2)
 
     }
 
     override fun onDisabled(context: Context) {
-        // Enter relevant functionality for when the last widget is disabled
 
         val name = ComponentName(context, Widget2::class.java)
         val appWidgetIds = AppWidgetManager.getInstance(context).getAppWidgetIds(name)
 
         if (appWidgetIds.isEmpty()) {
-            // stop alarm
             val appWidgetAlarm = AppWidgetAlarm1(context.applicationContext)
-            appWidgetAlarm.stopAlarm(1, Widget2::class.java, ACTION_AUTO_UPDATE)
+            appWidgetAlarm.stopAlarm(1, Widget2::class.java, ACTION_AUTO_UPDATE_W2)
         }
     }
 
     override fun onReceive(context: Context?, intent: Intent?) {
         super.onReceive(context, intent)
-        Log.e("OR CHK", "$intent")
+//        Log.e("OR CHK", "$intent")
 
         val name = context?.let { ComponentName(it, Widget2::class.java) }
         val appWidgetIds = AppWidgetManager.getInstance(context).getAppWidgetIds(name)
 
-        if (intent!!.action == ACTION_AUTO_UPDATE) {
+        if (intent!!.action == ACTION_AUTO_UPDATE_W2) {
             onUpdate(
                 context!!,
                 AppWidgetManager.getInstance(context),
@@ -61,7 +57,7 @@ class Widget2 : AppWidgetProvider() {
     }
 
     companion object {
-        const val ACTION_AUTO_UPDATE = "AUTO_UPDATE"
+        const val ACTION_AUTO_UPDATE_W2 = "AUTO_UPDATE"
     }
 }
 
@@ -70,12 +66,31 @@ internal fun updateAppWidget(
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
-    val widgetText = context.getString(R.string.appwidget_text)
-    // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.widget2)
     Log.e("WI2", "Called widget 2 Update")
-//    views.setTextViewText(R.id.appwidget_text, widgetText)
 
-    // Instruct the widget manager to update the widget
+    val widgetsDataHandlerDao = AppWidgetsDataHandlerDao()
+
+    val pref = context.getSharedPreferences("UsersSettingPref", Context.MODE_PRIVATE)
+    val langPref = pref.getInt("LanguagePreference", 0)
+
+    widgetsDataHandlerDao.loadJsonDataFromAssets(context, langPref)
+
+    val dayOfWeekText = widgetsDataHandlerDao.getCurrentDayOfWeek(langPref)
+    val date = widgetsDataHandlerDao.getTodaysDate(langPref)
+    val (tithiDate, zodiacSign) = widgetsDataHandlerDao.currentTithiAndZodiacSignForWidgets(
+        langPref
+    )
+    val hinduDate =
+        widgetsDataHandlerDao.currentHinduYearInFormatForWidgets() + " / " + tithiDate
+    val currentChoghadiya = widgetsDataHandlerDao.currentChoghadiya(langPref)
+
+    views.setTextViewText(R.id.dayTextViewW2, dayOfWeekText)
+    views.setTextViewText(R.id.dateTextViewW2, date)
+    views.setTextViewText(R.id.hinduDateW2, hinduDate)
+    views.setTextViewText(R.id.zodiacSignW2, zodiacSign)
+    views.setTextViewText(R.id.choghadiyaTextViewW2, "Choghadiya : $currentChoghadiya")
+
+
     appWidgetManager.updateAppWidget(appWidgetId, views)
 }
