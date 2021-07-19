883b2d2dce624754b69e53a5d875cd14bc233269
Raj varsani <varasaniraj5@gmail.com>
2021-07-19 14:00:44 +0530

    Added updating functionality to W5

--- Android/TheHinduCalender/app/src/main/java/com/example/thehinducalender/Widget5.kt
+++ Android/TheHinduCalender/app/src/main/java/com/example/thehinducalender/Widget5.kt
@@ -2,8 +2,12 @@ package com.example.thehinducalender
 
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.widget.RemoteViews
+import com.example.thehinducalender.appwidgetdaos.AppWidgetsDataHandlerDao
 
 /**
  * Implementation of App Widget functionality.
@@ -29,14 +33,38 @@ class Widget5 : AppWidgetProvider() {
     }
 
     override fun onEnabled(context: Context) {
-        // Enter relevant functionality for when the first widget is created
+        val appWidgetAlarm = AppWidgetAlarm1(context.applicationContext)
+        appWidgetAlarm.startAlarm(1, Widget5::class.java, Widget5.ACTION_AUTO_UPDATE_W5)
     }
 
     override fun onDisabled(context: Context) {
-        // Enter relevant functionality for when the last widget is disabled
+        val name = ComponentName(context, Widget5::class.java)
+        val appWidgetIds = AppWidgetManager.getInstance(context).getAppWidgetIds(name)
+
+        if (appWidgetIds.isEmpty()) {
+            val appWidgetAlarm = AppWidgetAlarm1(context.applicationContext)
+            appWidgetAlarm.stopAlarm(1, Widget5::class.java, ACTION_AUTO_UPDATE_W5)
+        }
     }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+//        Log.e("OR CHK", "$intent")
+
+        val name = context?.let { ComponentName(it, Widget5::class.java) }
+        val appWidgetIds = AppWidgetManager.getInstance(context).getAppWidgetIds(name)
+
+        if (intent!!.action == ACTION_AUTO_UPDATE_W5) {
+            onUpdate(
+                context!!,
+                AppWidgetManager.getInstance(context),
+                appWidgetIds
+            )
+        }
+    }
+
     companion object {
-        const val ACTION_AUTO_UPDATE = "AUTO_UPDATE"
+        const val ACTION_AUTO_UPDATE_W5 = "AUTO_UPDATE"
     }
 }
 
@@ -45,11 +73,35 @@ internal fun updateAppWidget5(
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
-    val widgetText = loadTitlePref5(context, appWidgetId)
-    // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.widget5)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
 
-    // Instruct the widget manager to update the widget
+    val views = RemoteViews(context.packageName, R.layout.widget5)
+    Log.e("WI5", "Called widget 5 Update")
+
+    val widgetsDataHandlerDao = AppWidgetsDataHandlerDao()
+
+    val pref = context.getSharedPreferences("UsersSettingPref", Context.MODE_PRIVATE)
+    val langPref = pref.getInt("LanguagePreference", 0)
+
+    widgetsDataHandlerDao.loadJsonDataFromAssets(context, langPref)
+
+    val dayOfWeekText = widgetsDataHandlerDao.getCurrentDayOfWeek(langPref)
+    val date = widgetsDataHandlerDao.getTodaysDate(langPref)
+    val (tithiDate, zodiacSign) = widgetsDataHandlerDao.currentTithiAndZodiacSignForWidgets(
+        langPref
+    )
+    val currentChoghadiya = widgetsDataHandlerDao.currentChoghadiya(langPref)
+    val nextChoghadiya = widgetsDataHandlerDao.nextChoghadiya(langPref)
+
+    views.setTextViewText(R.id.dayTextViewW5, dayOfWeekText)
+    views.setTextViewText(R.id.dateTextViewW5, date)
+    views.setTextViewText(
+        R.id.hinduDateW5P1,
+        widgetsDataHandlerDao.currentHinduYearInFormatForWidgets()
+    )
+    views.setTextViewText(R.id.hinduDateW5P2, tithiDate)
+    views.setTextViewText(R.id.zodiacSignW5, zodiacSign)
+    views.setTextViewText(R.id.choghadiyaTextViewW5, "$currentChoghadiya -> $nextChoghadiya")
+
+
     appWidgetManager.updateAppWidget(appWidgetId, views)
 }
