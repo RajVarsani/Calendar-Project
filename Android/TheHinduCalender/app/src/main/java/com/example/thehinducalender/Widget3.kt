8b7580e4dcc50bc33d7be8b31fc0c4a99e50874e
Raj varsani <varasaniraj5@gmail.com>
2021-07-19 14:00:21 +0530

    Added updating functionality to W3

--- Android/TheHinduCalender/app/src/main/java/com/example/thehinducalender/Widget3.kt
+++ Android/TheHinduCalender/app/src/main/java/com/example/thehinducalender/Widget3.kt
@@ -2,8 +2,12 @@ package com.example.thehinducalender
 
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.widget.RemoteViews
+import com.example.thehinducalender.appwidgetdaos.AppWidgetsDataHandlerDao
 
 /**
  * Implementation of App Widget functionality.
@@ -21,14 +25,39 @@ class Widget3 : AppWidgetProvider() {
     }
 
     override fun onEnabled(context: Context) {
-        // Enter relevant functionality for when the first widget is created
+        val appWidgetAlarm = AppWidgetAlarm1(context.applicationContext)
+        appWidgetAlarm.startAlarm(1, Widget3::class.java, ACTION_AUTO_UPDATE_W3)
+
     }
 
     override fun onDisabled(context: Context) {
-        // Enter relevant functionality for when the last widget is disabled
+        val name = ComponentName(context, Widget3::class.java)
+        val appWidgetIds = AppWidgetManager.getInstance(context).getAppWidgetIds(name)
+
+        if (appWidgetIds.isEmpty()) {
+            val appWidgetAlarm = AppWidgetAlarm1(context.applicationContext)
+            appWidgetAlarm.stopAlarm(1, Widget3::class.java, ACTION_AUTO_UPDATE_W3)
+        }
     }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+//        Log.e("OR CHK", "$intent")
+
+        val name = context?.let { ComponentName(it, Widget3::class.java) }
+        val appWidgetIds = AppWidgetManager.getInstance(context).getAppWidgetIds(name)
+
+        if (intent!!.action == ACTION_AUTO_UPDATE_W3) {
+            onUpdate(
+                context!!,
+                AppWidgetManager.getInstance(context),
+                appWidgetIds
+            )
+        }
+    }
+
     companion object {
-        const val ACTION_AUTO_UPDATE = "AUTO_UPDATE"
+        const val ACTION_AUTO_UPDATE_W3 = "AUTO_UPDATE"
     }
 }
 
@@ -37,10 +66,32 @@ internal fun updateAppWidget3(
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
-    val widgetText = context.getString(R.string.appwidget_text)
-    // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.widget3)
-//    views.setTextViewText(R.id.appwidget_text, widgetText)
+    Log.e("WI3", "Called widget 3 Update")
+
+    val widgetsDataHandlerDao = AppWidgetsDataHandlerDao()
+
+    val pref = context.getSharedPreferences("UsersSettingPref", Context.MODE_PRIVATE)
+    val langPref = pref.getInt("LanguagePreference", 0)
+
+    widgetsDataHandlerDao.loadJsonDataFromAssets(context, langPref)
+
+    val dayOfWeekText = widgetsDataHandlerDao.getCurrentDayOfWeek(langPref)
+    val date = widgetsDataHandlerDao.getTodaysDate(langPref)
+    val (tithiDate, zodiacSign) = widgetsDataHandlerDao.currentTithiAndZodiacSignForWidgets(
+        langPref
+    )
+    val hinduDate =
+        widgetsDataHandlerDao.currentHinduYearInFormatForWidgets() + " / " + tithiDate
+    val currentChoghadiya = widgetsDataHandlerDao.currentChoghadiya(langPref)
+    val nextChoghadiya = widgetsDataHandlerDao.nextChoghadiya(langPref)
+
+    views.setTextViewText(R.id.dayTextViewW3, dayOfWeekText)
+    views.setTextViewText(R.id.dateTextViewW3, date)
+    views.setTextViewText(R.id.hinduDateW3, hinduDate)
+    views.setTextViewText(R.id.zodiacSignW3, zodiacSign)
+    views.setTextViewText(R.id.choghadiyaTextViewW3, "$currentChoghadiya -> $nextChoghadiya")
+
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
