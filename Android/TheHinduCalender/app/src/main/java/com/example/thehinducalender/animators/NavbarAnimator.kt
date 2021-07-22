a24a07f286ea4b8c788412337dae8cab58873d30
Raj varsani <varasaniraj5@gmail.com>
2021-07-22 20:16:47 +0530

    Removed distance param from inactive function

--- Android/TheHinduCalender/app/src/main/java/com/example/thehinducalender/animators/NavbarAnimator.kt
+++ Android/TheHinduCalender/app/src/main/java/com/example/thehinducalender/animators/NavbarAnimator.kt
@@ -40,15 +40,14 @@ class NavbarAnimator {
         container: ConstraintLayout,
         iconImage1: ImageView,
         iconImage2: ImageView,
-        time_duration: Long,
-        distance: Int,
+        time_duration: Long
     ) {
 
 //        val animator1 = ObjectAnimator.ofFloat(iconImage1, "translationY", distance.toFloat())
 //        val animator2 = ObjectAnimator.ofFloat(iconImage2, "translationY", distance.toFloat())
         val animator3 = ObjectAnimator.ofFloat(iconImage1, "alpha", 0f, 1f)
         val animator4 = ObjectAnimator.ofFloat(iconImage2, "alpha", 1f, 0f)
-        val animator5 = ObjectAnimator.ofFloat(container, "translationY", distance.toFloat())
+        val animator5 = ObjectAnimator.ofFloat(container, "translationY",0f)
 
         (container.background as TransitionDrawable).startTransition(0)
         (container.background as TransitionDrawable).reverseTransition(time_duration.toInt())
