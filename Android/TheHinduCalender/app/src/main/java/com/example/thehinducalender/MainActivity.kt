5a60392450e1a5c4b71fd4c103d90afac1cbd71a
Raj varsani <varasaniraj5@gmail.com>
2021-07-26 12:58:38 +0530

    Updated opacity changes on onclick of navbar

--- Android/TheHinduCalender/app/src/main/java/com/example/thehinducalender/MainActivity.kt
+++ Android/TheHinduCalender/app/src/main/java/com/example/thehinducalender/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.thehinducalender
 
 import android.os.Bundle
+import android.os.Handler
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import com.example.thehinducalender.animators.NavbarAnimator
+import com.example.thehinducalender.animators.OpacityAnimator
 import com.example.thehinducalender.daos.DisplayDensityDao
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +18,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mainCalenderFragment: MainCalenderFragment
     private lateinit var widgetsListFragment: WidgetsListFragment
 
+    private lateinit var fragmentsArray: Array<Fragment>
+
     private lateinit var navbarAnimator: NavbarAnimator
+    private lateinit var opacityAnimator: OpacityAnimator
+
     private lateinit var displayDensityDao: DisplayDensityDao
 
     private var currentActiveFragment = 2
@@ -40,7 +47,10 @@ class MainActivity : AppCompatActivity() {
         festivalsAndHolidaysListFragment = FestivalsAndHolidaysListFragment()
         mainCalenderFragment = MainCalenderFragment()
         widgetsListFragment = WidgetsListFragment()
+
         navbarAnimator = NavbarAnimator()
+        opacityAnimator = OpacityAnimator()
+
         displayDensityDao = DisplayDensityDao()
 
         navbarButtons = arrayOf(
@@ -67,7 +77,16 @@ class MainActivity : AppCompatActivity() {
             findViewById(R.id.n_child_5_f),
         )
 
-        setUpFragment()
+        fragmentsArray = arrayOf(
+            festivalsAndHolidaysListFragment,
+            mainCalenderFragment,
+            homePageFragment,
+            widgetsListFragment,
+            homePageFragment
+        )
+
+
+        setUpThisFragment(2)
 
 
         navbarAnimator.setActive(
@@ -87,9 +106,7 @@ class MainActivity : AppCompatActivity() {
         for (i in navbarButtons.indices) {
             navbarButtons[i].setOnClickListener {
                 if (currentActiveFragment != i) {
-
-                 setThisCurrentFragment(i)
-
+                    setThisCurrentFragment(i)
                 }
             }
         }
@@ -97,6 +114,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setThisCurrentFragment(i: Int) {
+
+        opacityAnimator.setOpacityTo(mainFragmentFrameLayout, 100, 1f, 0f)
+
         navbarAnimator.setInactive(
             navbarButtons[currentActiveFragment],
             navbarButtonsInactiveImages[currentActiveFragment],
@@ -106,6 +126,8 @@ class MainActivity : AppCompatActivity() {
 
         currentActiveFragment = i
 
+
+
         navbarAnimator.setActive(
             navbarButtons[currentActiveFragment],
             navbarButtonsInactiveImages[currentActiveFragment],
@@ -113,13 +135,22 @@ class MainActivity : AppCompatActivity() {
             160,
             displayDensityDao.getPx(25)
         )
+
+
+        Handler(getMainLooper()).postDelayed({
+            setUpThisFragment(currentActiveFragment)
+            Handler(getMainLooper()).postDelayed({
+                opacityAnimator.setOpacityTo(mainFragmentFrameLayout, 100, 0f, 1f)
+            }, 100)
+        }, 160)
+
+
     }
 
-    private fun setUpFragment() {
+    private fun setUpThisFragment(index: Int) {
         supportFragmentManager.beginTransaction().apply {
-            replace(R.id.mainFragmentFrameLayout, homePageFragment)
+            replace(R.id.mainFragmentFrameLayout, fragmentsArray[index])
             commit()
         }
     }
-
 }
